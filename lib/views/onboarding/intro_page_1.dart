18f79f5127ef92f76f7060f15a9836ae350d0b26
Bilal Saeed <mianzainjh@gmail.com>
2026-08-29 10:28:49 +0500

    Now designing the onboarding page but still stuck on first page UI.

--- lib/views/onboarding/intro_page_1.dart
+++ lib/views/onboarding/intro_page_1.dart
@@ -5,8 +5,47 @@ class IntroPage1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.pink[100],
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.only(top: 40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ClipOval(
+                child: Image.asset(
+                  'assets/images/team.png',
+                  width: 280,
+                  height: 280,
+                  fit: BoxFit.cover,
+                ),
+              ),
+
+              const SizedBox(height: 30),
+
+              const Text(
+                'Make a difference in the \n lives of others.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              const SizedBox(height: 12),
+
+              const Text(
+                'Lets make the world batter place. One act of\n kindness at a time. ',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
