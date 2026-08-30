4dd9657870d9f6b9cead0eac490cade9a671e3b5
Bilal Saeed <mianzainjh@gmail.com>
2026-08-30 11:05:09 +0500

    I have successfully connected my project with firebase.And ready to start working on login screen..

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:charity_app/views/onboarding/onboarding_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 class MyApp extends StatelessWidget {
