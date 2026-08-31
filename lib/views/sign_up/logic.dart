77038ae57464b331acf2a40afd0c05947c93adcc
Bilal Saeed <mianzainjh@gmail.com>
2026-08-31 16:55:47 +0500

    I have build the half of logic for signup..

--- lib/views/sign_up/logic.dart
+++ lib/views/sign_up/logic.dart
@@ -1,5 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 class SignupController extends GetxController{
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController phoneController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  var isLoading = false.obs;
+
+  void SignUp() async{
+    String name = nameController.text.trim();
+    String email = emailController.text.trim();
+    String phone = phoneController.text.trim();
+    String password = passwordController.text.trim();
+
+    if (name.isEmpty || email.isEmpty || phone.isEmpty || password.isEmpty) {
+      Get.snackbar("Error", "All fields are required!", backgroundColor: Colors.red);
+      return;
+    }
+
+    isLoading.value = true; //Start loading....
+  }
 }
