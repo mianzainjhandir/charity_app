adec142459a10656ba9c0be2b5bb2db3920db7ae
Bilal Saeed <mianzainjh@gmail.com>
2026-09-02 17:08:10 +0500

    working on home screen.

--- lib/views/home/home_content.dart
+++ lib/views/home/home_content.dart
@@ -12,11 +12,42 @@ class _HomeContentState extends State<HomeContent> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text(
+          "Hello,\nEngineer Zain 👋",
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+
+        actions: [
+          IconButton(
+            onPressed: () {
+              // Notification screen open karni ho to yahan code likhein
+            },
+            icon: Icon(
+              Icons.notifications_none,
+              color: Colors.grey,
+              size: 28,
+            ),
+          ),
+        ],
+      ),
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.only(left: 14,right: 14,top: 8),
         child: Column(
           children: [
-            Text(" Hello,\n Engr Zain👋",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),)
+            SizedBox(
+              width: double.infinity,
+              height: 200, // 👈 height yahan se adjust karein
+              child: Image.asset(
+                'assets/images/banner.png',
+                fit: BoxFit.cover,
+              ),
+            ),
+
           ],
         ),
       ),
