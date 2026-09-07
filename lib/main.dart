633294d4a5fe21c212d8b8418de7d9508e16cea8
Bilal Saeed <mianzainjh@gmail.com>
2026-09-07 12:58:48 +0500

    Alhamdulilah, I have completed this charity app.

--- lib/main.dart
+++ lib/main.dart
@@ -40,3 +40,4 @@ class MyApp extends StatelessWidget {
 // Fixed the error: Fix Git push failure with HTTP 408 RPC error.
 // Added some more campaigns
 // I have complete this charity app.
+// now my next step is to test this app on my android phone.
